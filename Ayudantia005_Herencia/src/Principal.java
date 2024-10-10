6be45c01b2651340d701dee7a15df53ed6574d94
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-10-10 09:00:04 -0300

    update

---
+++ Ayudantia005_Herencia/src/Principal.java
@@ -0,0 +1,30 @@
+    /*
+    Crear las clases Producto con los atributos: id, nombre, marca y precio. Adicionalmente
+    2 Subclases Teclado ( cantidad de teclas y si es mecanico) y Mouse 
+    (cantidad de botones y si es optico).
+    
+    
+        Ejercicio 1: Crear un objeto teclado, completar con
+    datos y mostrarlos en la consola.
+    
+        Ejercicio 2: Crear la clase Mouse. Heredar de la clase
+    Producto y agregar los atributos: esOptico y botones.
+    
+        Ejercicio 3: Crear un objeto mouse, completar con 
+    datos y mostrarlos en la consola.
+    
+        Ejercicio 4: Crear un nuevo teclado y solicitar los
+    datos por teclado. (usar Scanner)
+    Leer ppt 2.1.1 y 2.2.1
+    
+    */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
