b029e65b3e787c2b37ad3a1822f2b9a3fd0cdb2b
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-10-24 09:34:14 -0300

    update

--- EjercicioTipoPrueba2_005/src/Moto.java
+++ EjercicioTipoPrueba2_005/src/Moto.java
@@ -1,13 +1,66 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author patri
- */
-public class Moto {
+public final class Moto extends Vehiculo {
+    private String estilo;
+    private int medidaManillar;
+
+    public Moto() {
+        this.estilo = "";
+        this.medidaManillar = 0;
+    }
+
+    public Moto(String estilo, int medidaManillar, String patente, String rut, int precioServicioNeto, int iva, int totalPorPagar) {
+        super(patente, rut, precioServicioNeto, iva, totalPorPagar);
+        this.estilo = estilo;
+        this.medidaManillar = medidaManillar;
+    }
+
+    public int getMedidaManillar() {
+        return medidaManillar;
+    }
+
+    public void setMedidaManillar(int medidaManillar) {
+        this.medidaManillar = medidaManillar;
+    }
+
+    public String getEstilo() {
+        return estilo;
+    }
+
+    public void setEstilo(String estilo) {
+        this.estilo = estilo;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + 
+                "estilo=" + estilo + 
+                ", medidaManillar=" + medidaManillar;
+    }
+
+    @Override
+    public void totalPorPagar() {
+        int resultado = this.getPrecioServicioNeto()+ this.getIva();
+        this.setTotalPorPagar(resultado);
+    }
+
+    @Override
+    public void totalConIVA() {
+        double resultado = this.getPrecioServicioNeto() * IVA;
+        this.setIva((int)resultado);
+    }
+
+    @Override
+    public void obtenerDescuento() {
+        double resultado = this.getPrecioServicioNeto() * DESCUENTO_MOTO;
+        resultado = this.getPrecioServicioNeto() - resultado ;
+        this.setPrecioServicioNeto((int)resultado);
+        
+    }
+    public void obtenerDescuento(int descuento) {
+        double resultado = this.getPrecioServicioNeto() * descuento;
+        resultado = this.getPrecioServicioNeto() - resultado ;
+        this.setPrecioServicioNeto((int)resultado);
+        
+    }
     
 }
--- EjercicioTipoPrueba2_005/src/Vehiculo.java
+++ EjercicioTipoPrueba2_005/src/Vehiculo.java
@@ -1,10 +1,76 @@
 
 public abstract class Vehiculo implements IOperaciones {
-    String patente;
-    String rut;
-    int precioServicioNeto;
-    int iva;
-    int totalPorPagar;
+    private String patente;
+    private String rut;
+    private int precioServicioNeto;
+    private int iva;
+    private int totalPorPagar;
+
+    public Vehiculo() {
+        this.patente = "";
+        this.rut = "";
+        this.precioServicioNeto = 0;
+        this.iva = 0;
+        this.totalPorPagar = 0;
+    }
+        
+    public Vehiculo(String patente, String rut, int precioServicioNeto, int iva, int totalPorPagar) {
+        this.patente = patente;
+        this.rut = rut;
+        this.precioServicioNeto = precioServicioNeto;
+        this.iva = iva;
+        this.totalPorPagar = totalPorPagar;
+    }
+
+    public int getTotalPorPagar() {
+        return totalPorPagar;
+    }
+
+    public void setTotalPorPagar(int totalPorPagar) {
+        this.totalPorPagar = totalPorPagar;
+    }
+
+    public String getPatente() {
+        return patente;
+    }
+
+    public void setPatente(String patente) {
+        this.patente = patente;
+    }
+
+    public String getRut() {
+        return rut;
+    }
+
+    public void setRut(String rut) {
+        this.rut = rut;
+    }
+
+    public int getPrecioServicioNeto() {
+        return precioServicioNeto;
+    }
+
+    public void setPrecioServicioNeto(int precioServicioNeto) {
+        this.precioServicioNeto = precioServicioNeto;
+    }
+
+    public int getIva() {
+        return iva;
+    }
+
+    public void setIva(int iva) {
+        this.iva = iva;
+    }
+
+    @Override
+    public String toString() {
+        return "patente=" + patente + 
+                ", rut=" + rut + 
+                ", precioServicioNeto=" + precioServicioNeto + 
+                ", iva=" + iva + 
+                ", totalPorPagar=" + totalPorPagar;
+    }
     
+    public abstract void totalPorPagar();
     
 }
